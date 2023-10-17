16c53ca1ffa9222899656eb1a9f3e630634759c3
jeffmlb91 <jeffmlb91@gmail.com>
2023-10-16 23:55:01 -0400

    In this section, we learn about how a basic function can be used to return expected result when passing the correct parameters type. We also learn how passing string can return a concatinated string instead of adding  the numbers

---
+++ index.js
@@ -0,0 +1,12 @@
+//regular function
+const add = (a, b) => {
+   return a + b
+}
+console.log(add(2, 3))
+
+// Second fubnctiion with error
+//User intry adds string instead of numbers
+const addTwo = (a, b) => {
+    return a + b;
+}
+console.log(addTwo('3', '5'))
